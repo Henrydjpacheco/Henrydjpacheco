9382f68a2eb82ec6d6a3c377a9dfb4fbf6bd2d20
henrydjpacheco <henrydjpacheco@gmailcom>
2023-02-28 22:49:50 -0500

    v1.0.4

--- api/src/db.js
+++ api/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
-
+   console.log(DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
 const sequelize = new Sequelize(
    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
    {
