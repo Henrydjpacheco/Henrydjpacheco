7c0339c3e63033d8dde1f1685e7517c8ad276949
Henry Pacheco <77772835+Henrydjpacheco@users.noreply.github.com>
2023-06-21 21:00:11 -0500

    Delete client directory

--- client/src/redux/store/index.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from '../reducer';
-
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
-
-export default store;
